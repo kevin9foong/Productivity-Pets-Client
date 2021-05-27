7941b40bce241ac6f67e6312966d6f8fe3622465
zacchua <zacchua@gmail.com>
2021-05-27 16:23:59 +0800

    Integrated Kitten UI component library
    Integrating Kitten UI requires a workaround, which involves
    running the command 'expo install @expo/webpack-config' and
    including the webpack.config.js file at the root file of the
    project.

--- app/app.tsx
+++ app/app.tsx
@@ -1,30 +1,32 @@
-import { registerRootComponent } from 'expo';
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { registerRootComponent } from "expo";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import * as eva from "@eva-design/eva";
+import { ApplicationProvider, Layout, Text } from "@ui-kitten/components";
 
-import SignUp from './pages/signup';
-import LoginPage from './pages/loginpage';
+import SignUp from "./pages/signup";
+import LoginPage from "./pages/loginpage";
 
 type Props = {};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
-    fontSize: 18
-  }
+    fontSize: 18,
+  },
 });
 
 const App: React.FC<Props> = () => {
-  const [page, togglePage] = React.useState('login');
+  const [page, togglePage] = React.useState("login");
 
   const pageToDisplay = () => {
-    if (page === 'signup') {
+    if (page === "signup") {
       return <SignUp handlePage={togglePage} />;
     } else {
       return <LoginPage handlePage={togglePage} />;
@@ -35,11 +37,13 @@ const App: React.FC<Props> = () => {
   // a folder within a pages directory perhaps. Start working on home pages
   // integrate Kitten UI
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Welcome to Productivity Pets!</Text>
-      {pageToDisplay()}
-      <StatusBar style='auto' />
-    </View>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Welcome to Productivity Pets!</Text>
+        {pageToDisplay()}
+        <StatusBar style="auto" />
+      </View>
+    </ApplicationProvider>
   );
 };
 
