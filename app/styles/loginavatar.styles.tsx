2d7aa882a30a2fbde59ffc9f585799416f37d8b6
Kevin Foong <kevin9foong@gmail.com>
2021-05-29 19:30:28 +0800

    Add google login component

---
+++ app/styles/loginavatar.styles.tsx
@@ -0,0 +1,27 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  content: {
+    flex: 3,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  header: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  },
+  avatar: {
+    margin: 20
+  },
+  avatarImage: {
+    borderRadius: 50,
+    height: 100,
+    width: 100
+  },
+  text: {
+    textAlign: 'center',
+    color: '#333',
+    marginBottom: 5
+  }
+});
