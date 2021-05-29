d6f01946f17bad94f487078e35831551d4136a2f
Kevin Foong <kevin9foong@gmail.com>
2021-05-29 19:30:28 +0800

    Add google login component

---
+++ app/components/common/iconbutton.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  View
+} from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+import styles from '../../styles/iconbutton.styles';
+
+type IconButtonProps = {
+    iconName: keyof typeof FontAwesome.glyphMap,
+    handleButtonPress: () => void,
+    buttonText: string
+};
+
+const IconButton: React.FC<IconButtonProps> = ({ iconName, handleButtonPress, buttonText }: IconButtonProps) => {
+  return (
+        <View style={styles.container}>
+            <View style={styles.buttons}>
+                <FontAwesome.Button
+                    name={iconName}
+                    backgroundColor='#DD4B39'
+                    onPress={handleButtonPress}>
+                        {buttonText}
+                </FontAwesome.Button>
+            </View>
+        </View>
+  );
+};
+
+export default IconButton;
---
+++ app/components/login/loginavatar.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+  Image,
+  Text,
+  View
+} from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+import styles from '../../styles/loginavatar.styles';
+
+type LoginAvatarProps = {
+    greetingMessage: string,
+    avatarImgSource: string | undefined,
+    actionMessage?: string | undefined | null
+};
+
+const LoginAvatar: React.FC<LoginAvatarProps> = ({ greetingMessage, avatarImgSource, actionMessage }: LoginAvatarProps) => {
+  return (
+        <View style={styles.content}>
+            <Text style={styles.header}>
+                {greetingMessage}
+            </Text>
+            <View style={styles.avatar}>
+                {avatarImgSource
+                  ? <Image source={{ uri: avatarImgSource }} style={styles.avatarImage} />
+                  : <FontAwesome name="user-circle" size={100} color="rgba(0,0,0,.09)" />}
+            </View>
+            {actionMessage
+              ? <Text style={styles.text}>{actionMessage}</Text>
+              : null}
+        </View>
+  );
+};
+
+export default LoginAvatar;
